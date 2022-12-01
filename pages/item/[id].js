9768e9b36bee7c67e1116c7aa6ee131a4d442b8f
Niyasin <mo.niyas2000@gmail.com>
2022-12-01 23:53:40 +0530

    details 💯

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -9,8 +9,11 @@ export default function Item (){
             name:'Product Name',
             prize:'50',
             offers:'12%',
+            rating:4.5,
+            description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         }
     );
+    
     const [images,setImages]=useState(['../sample.png','../sample.png','../sample.png','../sample.png','../sample.png']);
     const [selectedImage,setSelectedImages]=useState('../sample.png');
 return(
@@ -25,7 +28,19 @@ return(
                 })}
             </div>
         </div>
-        <div className="details"></div>
+        <div className="details">
+            <h1>{data.name}</h1>
+            <h3>${data.prize}</h3>
+            <p>{data.description}</p>
+            <h1>Rating</h1>
+            <div className="horizontal">
+                <h4>{data.rating}</h4>
+            </div>
+            <div className="horizontal">
+            <div className="button">Add To Cart</div>
+            <div className="button">Buy Now</div>
+            </div>
+        </div>
     </div>
 )
 }
