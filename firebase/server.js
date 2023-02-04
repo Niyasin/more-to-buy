ea49e0af969c480e456a6e3ae2decea387a01ffb
Niyasin <mo.niyas2000@gmail.com>
2023-02-04 17:08:13 +0530

    fix 2

--- firebase/server.js
+++ firebase/server.js
@@ -4,7 +4,7 @@ const ServiceAccount ={
   type:process.env.ADMIN_TYPE,
   project_id:process.env.ADMIN_PROJECT_ID,
   private_key_id:process.env.ADMIN_PRIVATE_KEY_ID,
-  private_key:process.env.ADMIN_PRIVATE_KEY,
+  private_key:process.env.ADMIN_PRIVATE_KEY.replace(/\\n/g,'\n'),
   client_email:process.env.ADMIN_CLIENT_EMAIL,
   client_id:process.env.ADMIN_CLIENT_ID,
   auth_uri:process.env.ADMIN_AUTH_URI,
@@ -12,6 +12,7 @@ const ServiceAccount ={
   auth_provider_x509_cert_url:process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL,
   client_x509_cert_url:process.env.ADMIN_CLIENT_X509_CERT_URL,
 }
+console.log(ServiceAccount.private_key);
 try{
   getApp('server');
 }catch(e){
