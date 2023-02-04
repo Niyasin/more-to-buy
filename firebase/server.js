426f09480bbd85aa0768ab6999db82a538133b21
Niyasin <mo.niyas2000@gmail.com>
2023-02-04 16:26:26 +0530

    fix

--- firebase/server.js
+++ firebase/server.js
@@ -1,6 +1,17 @@
 import  {initializeApp,getApp} from 'firebase-admin/app';
 import {credential} from 'firebase-admin';
-import  ServiceAccount  from '../serviceKey.json';
+const ServiceAccount ={
+  type:process.env.ADMIN_TYPE,
+  project_id:process.env.ADMIN_PROJECT_ID,
+  private_key_id:process.env.ADMIN_PRIVATE_KEY_ID,
+  private_key:process.env.ADMIN_PRIVATE_KEY,
+  client_email:process.env.ADMIN_CLIENT_EMAIL,
+  client_id:process.env.ADMIN_CLIENT_ID,
+  auth_uri:process.env.ADMIN_AUTH_URI,
+  token_uri:process.env.ADMIN_TOKEN_URI,
+  auth_provider_x509_cert_url:process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url:process.env.ADMIN_CLIENT_X509_CERT_URL,
+}
 try{
   getApp('server');
 }catch(e){
